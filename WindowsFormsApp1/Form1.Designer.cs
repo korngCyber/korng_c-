8441a088ae690e41ddcfcd0897e860505a9e9859
korngCyber <iamkorng110@gmail.com>
2024-01-04 10:43:38 +0700

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pic_box = new System.Windows.Forms.PictureBox();
+            this.txt_bookQty = new System.Windows.Forms.TextBox();
+            this.txt_bookName = new System.Windows.Forms.TextBox();
+            this.txt_bookID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.btn_previous = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic_box
+            // 
+            this.pic_box.Image = global::WindowsFormsApp1.Properties.Resources.noImage;
+            this.pic_box.Location = new System.Drawing.Point(361, 235);
+            this.pic_box.Name = "pic_box";
+            this.pic_box.Size = new System.Drawing.Size(391, 233);
+            this.pic_box.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic_box.TabIndex = 0;
+            this.pic_box.TabStop = false;
+            this.pic_box.Click += new System.EventHandler(this.pic_box_Click);
+            // 
+            // txt_bookQty
+            // 
+            this.txt_bookQty.Location = new System.Drawing.Point(361, 156);
+            this.txt_bookQty.Multiline = true;
+            this.txt_bookQty.Name = "txt_bookQty";
+            this.txt_bookQty.Size = new System.Drawing.Size(391, 41);
+            this.txt_bookQty.TabIndex = 1;
+            // 
+            // txt_bookName
+            // 
+            this.txt_bookName.Location = new System.Drawing.Point(361, 89);
+            this.txt_bookName.Multiline = true;
+            this.txt_bookName.Name = "txt_bookName";
+            this.txt_bookName.Size = new System.Drawing.Size(391, 44);
+            this.txt_bookName.TabIndex = 1;
+            // 
+            // txt_bookID
+            // 
+            this.txt_bookID.Location = new System.Drawing.Point(361, 31);
+            this.txt_bookID.Multiline = true;
+            this.txt_bookID.Name = "txt_bookID";
+            this.txt_bookID.Size = new System.Drawing.Size(391, 40);
+            this.txt_bookID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(208, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Book_ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Book_Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(208, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Book Qty";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(208, 344);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 29);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Picture Box";
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(800, 156);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(100, 40);
+            this.btn_add.TabIndex = 3;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_next
+            // 
+            this.btn_next.Location = new System.Drawing.Point(931, 590);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(100, 40);
+            this.btn_next.TabIndex = 3;
+            this.btn_next.Text = "Next";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // btn_previous
+            // 
+            this.btn_previous.Location = new System.Drawing.Point(12, 590);
+            this.btn_previous.Name = "btn_previous";
+            this.btn_previous.Size = new System.Drawing.Size(126, 40);
+            this.btn_previous.TabIndex = 3;
+            this.btn_previous.Text = "Previous";
+            this.btn_previous.UseVisualStyleBackColor = true;
+            this.btn_previous.Click += new System.EventHandler(this.btn_previous_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(800, 338);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(100, 40);
+            this.btn_delete.TabIndex = 3;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth24Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(256, 256);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 642);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_previous);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_bookID);
+            this.Controls.Add(this.txt_bookName);
+            this.Controls.Add(this.txt_bookQty);
+            this.Controls.Add(this.pic_box);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic_box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic_box;
+        private System.Windows.Forms.TextBox txt_bookQty;
+        private System.Windows.Forms.TextBox txt_bookName;
+        private System.Windows.Forms.TextBox txt_bookID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.Button btn_previous;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.ImageList imageList;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private class Books
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string qty { get; set; }
+            public string images { get; set; }
+        };
+        List<Books> books = new List<Books>();
+        int count=-1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void pic_box_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of=new OpenFileDialog();
+            DialogResult pic_selected= of.ShowDialog();
+            if(pic_selected == DialogResult.OK) 
+            {
+                string image_path = of.FileName;
+                pic_box.Image = Image.FromFile(image_path);
+                pic_box.Tag = image_path; // Reset the Tag after using the image
+                Books book = new Books
+                {
+                    images = image_path
+                };
+                books.Add(book);
+            }
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                if((pic_box.Tag == null || pic_box.Tag.Equals(Properties.Resources.noImage)))
+                {
+                    MessageBox.Show(" Please Input Image first! ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                Books current_book = books[books.Count - 1];
+                current_book.Id = txt_bookID.Text.Trim();
+                current_book.Name = txt_bookID.Text.Trim();
+                current_book.qty = txt_bookQty.Text.Trim();
+                txt_bookID.Clear();
+                txt_bookName.Clear();
+                txt_bookQty.Clear();
+                pic_box.Image = Properties.Resources.noImage;
+                pic_box.Tag = null;// Use Tag to store the image path
+            }
+            catch(Exception)
+            {
+                MessageBox.Show(" Please Input Image first! ","Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            navigate(1);
+        }
+        private void navigate(int direction)
+        {
+            try 
+            {
+                int imageIndex = books.Count - 1;
+                count += direction;
+                if (count < 0)
+                {
+                    count = 0;
+                }
+                else if (count > imageIndex)
+                {
+                    count = imageIndex;
+                }
+                txt_bookID.Text=books[count].Id.ToString();
+                txt_bookName.Text=books[count].Name.ToString();
+                txt_bookQty.Text=books[count].qty.ToString();
+                pic_box.Image = Image.FromFile(books[count].images);
+            }
+            catch(ArgumentOutOfRangeException)
+            {
+                MessageBox.Show(" Please Input image first. ","error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+        private void btn_previous_Click(object sender, EventArgs e)
+        {
+            navigate(-1);
+        }
+    }
+}
